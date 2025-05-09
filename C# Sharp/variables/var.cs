bf557d8225222ebb7ad1b6eb35e60608338e5b36
ojijo123 <oumaerick173@gmail.com>
2025-05-09 10:10:50 +0300

    C sharp variable

---
+++ C# Sharp/variables/var.cs
@@ -0,0 +1,16 @@
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string name = "Erick Ojijo"; // String variable
+            int age = 26; // Integer variable
+            double height = 1.75; // Double variable
+            bool isStudent = false; // Boolean variable
+            char initial = 'E'; // Character variable
+            string[] hobbies = { "Reading", "Traveling", "Coding" }; // Array of strings
+        }
+    }
+}
